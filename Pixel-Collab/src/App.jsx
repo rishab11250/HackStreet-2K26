e6a0c5c07ed0326dbf39eecfeb585f9094e53347
vineet1cg <vineet.prajapati.cg@gmail.com>
2026-05-03 12:22:36 +0530

    added keyboard functionality

--- Pixel-Collab/src/App.jsx
+++ Pixel-Collab/src/App.jsx
@@ -2,9 +2,11 @@ import AppLayout from './components/layout/AppLayout'
 import WhiteboardCanvas from './canvas/WhiteboardCanvas'
 import MockCursors from './components/collaboration/MockCursors'
 import { useMockCollaboration } from './hooks/useMockCollaboration'
+import { useKeyboard } from './hooks/useKeyboard'
 
 function App() {
   useMockCollaboration();
+  useKeyboard();
 
   return (
     <AppLayout>
